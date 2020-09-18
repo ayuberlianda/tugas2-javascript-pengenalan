9670ba60d588c297d803f5c9ed85994cdfd875ff
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-09-18 10:57:41 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,17 @@
+var nama = "Ayu Berlianda"
+var NIP = 201902
+var statusMenikah = true
+var statusAnak = false
+var data = ["Wanita","DPMPTSP","Bekerja","Drakor"]
+var Umur = "29"
+var absen = "ada"
+absen = null
+
+
+console.log(nama)
+console.log(NIP)
+console.log(statusMenikah)
+console.log(statusAnak)
+console.log(data [1])
+console.log(Umur)
+console.log(absen)
